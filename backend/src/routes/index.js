7c5317b4e5a14b2d1d49bec718fd8c71a9754123
Leonardo Leal <leonardotade@gmail.com>
2020-03-26 23:00:35 -0300

    arquivos aula 2 (backend)

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,12 +1,21 @@
 const express = require('express');
 
+const OngController = require('../modules/ongs/OngController');
+const IncidentsController = require('../modules/incidents/IncidentController');
+const ProfileController = require('../modules/profiles/ProfileController');
+const SessionsController = require('../modules/sessions/SessionController');
+
 const routes = express.Router();
 
-routes.get('/users', (request, response) => {
-  return response.json({
-    evento: 'Semana OmniStack 11',
-    aluno: 'Leonardo Leal'
-  });
-});
+routes.post('/sessions', SessionsController.create);
+
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentsController.index);
+routes.post('/incidents', IncidentsController.create);
+routes.delete('/incidents/:id', IncidentsController.delete);
+
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
