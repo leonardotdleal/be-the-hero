f301cc2d6689425a4ab14375e2116cc862667f2d
Leonardo Leal <leonardotade@gmail.com>
2020-03-27 21:45:38 -0300

    front-end da aplicação

---
+++ frontend/src/components/Link/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link as LinkReactDom } from 'react-router-dom';
+
+import './styles.css';
+
+function Link({ children, label, to }) {
+    return (
+        <LinkReactDom className="back-link" to={to}>
+            <span className="back-link-icon">
+                { children }
+            </span>
+            <span>{ label }</span>
+        </LinkReactDom>
+    )
+}
+
+export default React.memo(Link);
