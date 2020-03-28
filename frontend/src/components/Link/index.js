5726d8b5d17600877246806d7c0c54e77f13adec
Leonardo Leal <leonardotade@gmail.com>
2020-03-28 14:49:05 -0300

    configure eslint, prettier and editorconfig

--- frontend/src/components/Link/index.js
+++ frontend/src/components/Link/index.js
@@ -4,14 +4,12 @@ import { Link as LinkReactDom } from 'react-router-dom';
 import './styles.css';
 
 function Link({ children, label, to }) {
-    return (
-        <LinkReactDom className="back-link" to={to}>
-            <span className="back-link-icon">
-                { children }
-            </span>
-            <span>{ label }</span>
-        </LinkReactDom>
-    )
+  return (
+    <LinkReactDom className="back-link" to={to}>
+      <span className="back-link-icon">{children}</span>
+      <span>{label}</span>
+    </LinkReactDom>
+  );
 }
 
 export default React.memo(Link);
--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -7,16 +7,16 @@ import Profile from './pages/Profile';
 import NewIncident from './pages/NewIncident';
 
 function Routes() {
-    return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Logon}/>
-                <Route path="/register" component={Register}/>
-                <Route path="/profile" component={Profile}/>
-                <Route path="/incidents/new" component={NewIncident}/>
-            </Switch>
-        </BrowserRouter>
-    )
-};
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Logon} />
+        <Route path="/register" component={Register} />
+        <Route path="/profile" component={Profile} />
+        <Route path="/incidents/new" component={NewIncident} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
 export default Routes;
